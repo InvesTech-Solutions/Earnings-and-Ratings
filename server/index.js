c0e5e08fd26092aadb7bc92ef1e285409067a9f5
Jae <jaebae1111@gmail.com>
2018-07-20 15:39:39 -0700

    adding fake data request to the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('../database/db');
 
 let port = 3036;
 let app = express();
@@ -8,6 +9,11 @@ let app = express();
 app.use(express.static(__dirname + '/../client/dist/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/fakeData', (req, res) => {
+  console.log('dfdfdfffdff');
+  res.json();
+})
 
 app.listen(port, function() {
 	console.log(`listening on port ${port}`);
