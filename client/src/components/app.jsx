6c9b86fc89c5583830802128ada46b53a605919b
Jae <jaebae1111@gmail.com>
2018-07-23 17:45:37 -0700

    finished writing tests for basic App component data loading specs

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import $ from 'jquery';
-// console.log(data);
+
 class App extends React.Component {
   constructor(props) {
     super(props);
--- client/src/specs/App.test.js
+++ client/src/specs/App.test.js
@@ -3,10 +3,22 @@ import { shallow, mount, render } from 'enzyme';
 import App from './app.js';
 
 describe('component: App', () => {
-  it('should render', () => {
-    const wrapper = shallow(<App />);
-    const exampleLoaded = 1;
-    console.log(exampleLoaded);
-    expect(exampleLoaded).toEqual(1)
+  it('should render initial layout', () => {
+    let wrapper = shallow(<App />);
+    expect(wrapper.getElements()).toMatchSnapshot();
+  });
+
+  it('should not have any preexisting data loaded for the app', () => {
+    let wrapper = shallow(<App />);
+    let loadState = wrapper.state().exampleDataLoaded;
+    expect(loadState).toEqual(false);
+  });
+
+  it('should load data from the database once component was loaded', () => {
+    let wrapper = shallow(<App />);
+    setTimeout(() => {
+      let loadState = wrapper.state().exampleDataLoaded;
+      expect(loadState).toEqual(true);
+    }, 0);
   });
 });
